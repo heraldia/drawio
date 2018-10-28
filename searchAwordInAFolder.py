516a84fe468f29bd45ae7698a8305ac618d15d7a
Phil Feng <yunfei.feng@samsclub.com>
2018-10-28 09:07:38 -0500

    todo compressed default

--- searchAwordInAFolder.py
+++ searchAwordInAFolder.py
@@ -2,11 +2,10 @@ import os
 import sys
 import platform
 
-def searchAWord(filename, old_str):
+def searchAWord(filename, old_str, sysstr):
     file_data = open("{}.codereview".format(old_str), 'a+')
     WROTEFILENAME = False
 
-    sysstr = platform.system()
     if(sysstr =="Windows"):
         with open(filename, "r", encoding='gb18030', errors='ignore') as f:
             lineCounter = 0
@@ -14,11 +13,10 @@ def searchAWord(filename, old_str):
                 lineCounter += 1
                 if old_str in line:
                     if not WROTEFILENAME: 
-                        file_data.write("#e "+filename+'\n')
+                        file_data.write("e "+filename+'\n')
                         WROTEFILENAME = True
-                    file_data.write(str(lineCounter) + ": " + line)
-        file_data.write('\n')
-        file_data.close()
+                    file_data.write("\t"+str(lineCounter) + ": " + line)
+            #if WROTEFILENAME: file_data.write('\n')
 
     elif(sysstr[0] == "D"): # macbook
         with open(filename, "r") as f:
@@ -27,16 +25,18 @@ def searchAWord(filename, old_str):
                 lineCounter += 1
                 if old_str in line:
                     if not WROTEFILENAME: 
-                        file_data.write("#e "+filename+'\n')
+                        file_data.write("e "+filename+'\n')
                         WROTEFILENAME = True
-                    file_data.write(str(lineCounter) + ": " + line)
-        file_data.write('\n')
-        file_data.close()
+                    file_data.write("\t"+str(lineCounter) + ": " + line)
+            #if WROTEFILENAME: file_data.write('\n')
+
+    file_data.close()
 
 def traverseFolder(search_folder,searchStr):
+    sysstr = platform.system()
     for dirpath, dirnames, filenames in os.walk(search_folder): # using
         for filename in filenames:
             if '.js' in filename or '.java' in filename:
-                searchAWord(os.path.join(dirpath,filename), searchStr)
+                searchAWord(os.path.join(dirpath,filename), searchStr, sysstr)
 
 traverseFolder('.', sys.argv[1])
