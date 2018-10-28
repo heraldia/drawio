6ca62979506ff3cd438e258c1fa3cdaeedb28c1c
Phil Feng <yunfei.feng@samsclub.com>
2018-10-27 19:45:15 -0500

    paperSize7inch

---
+++ searchAwordInAFolder.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+def searchAWord(filename, old_str):
+    file_data = open("{}.codereview".format(old_str), 'a+')
+    WROTEFILENAME = False
+    with open(filename, "r", encoding='gb18030', errors='ignore') as f:
+        lineCounter = 0
+        for line in f:
+            lineCounter += 1
+            if old_str in line:
+                if not WROTEFILENAME: 
+                    file_data.write("# "+filename+'{\n')
+                    WROTEFILENAME = True
+                file_data.write(str(lineCounter) + ": " + line)
+    if WROTEFILENAME: file_data.write('}\n')
+    file_data.close()
+
+def traverseFolder(search_folder,searchStr):
+    for dirpath, dirnames, filenames in os.walk(search_folder): # using
+        for filename in filenames:
+            if '.js' in filename or '.java' in filename:
+                searchAWord(os.path.join(dirpath,filename), searchStr)
+
+traverseFolder('.', sys.argv[1])
