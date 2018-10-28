59e7106a4a0c458b9143a4195f5d4eb85a9b08a4
Phil Feng <yunfei.feng@samsclub.com>
2018-10-27 22:34:44 -0500

    app.min.js 11665 default letter size

--- searchAwordInAFolder.py
+++ searchAwordInAFolder.py
@@ -1,20 +1,35 @@
 import os
 import sys
+import platform
 
 def searchAWord(filename, old_str):
     file_data = open("{}.codereview".format(old_str), 'a+')
     WROTEFILENAME = False
-#with open(filename, "r", encoding='gb18030', errors='ignore') as f:
-    with open(filename, "r") as f:
-        lineCounter = 0
-        for line in f:
-            lineCounter += 1
-            if old_str in line:
-                if not WROTEFILENAME: 
-                    file_data.write("#e "+filename+'\n')
-                    WROTEFILENAME = True
-                file_data.write(str(lineCounter) + ": " + line)
-    file_data.close()
+
+    sysstr = platform.system()
+    if(sysstr =="Windows"):
+        with open(filename, "r", encoding='gb18030', errors='ignore') as f:
+            lineCounter = 0
+            for line in f:
+                lineCounter += 1
+                if old_str in line:
+                    if not WROTEFILENAME: 
+                        file_data.write("#e "+filename+'\n')
+                        WROTEFILENAME = True
+                    file_data.write(str(lineCounter) + ": " + line)
+        file_data.close()
+
+    elif(sysstr[0] == "D"): # macbook
+        with open(filename, "r") as f:
+            lineCounter = 0
+            for line in f:
+                lineCounter += 1
+                if old_str in line:
+                    if not WROTEFILENAME: 
+                        file_data.write("#e "+filename+'\n')
+                        WROTEFILENAME = True
+                    file_data.write(str(lineCounter) + ": " + line)
+        file_data.close()
 
 def traverseFolder(search_folder,searchStr):
     for dirpath, dirnames, filenames in os.walk(search_folder): # using
